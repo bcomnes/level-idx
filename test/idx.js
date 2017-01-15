07ee8bacb59a037b6a586828881e38859687a3bb
Bret Comnes <bcomnes@gmail.com>
2017-01-15 13:51:07 -0800

    add bytewise test

--- test/idx.js
+++ test/idx.js
@@ -2,6 +2,7 @@ var MemDB = require('memdb')
 var Index = require('..')
 var sub = require('subleveldown')
 var test = require('tape')
+var bytewise = require('bytewise')
 
 test('idx', function (t) {
   t.plan(7)
@@ -35,3 +36,34 @@ test('idx', function (t) {
   })
 })
 
+test('idx-bytewise', function (t) {
+  t.plan(7)
+
+  var db = MemDB('./db')
+  var posts = sub(db, 'posts', {keyEncoding: bytewise, valueEncoding: 'json'})
+  var idb = sub(db, 'index')
+
+  Index(posts, idb, {keyEncoding: bytewise})
+    .by('Title', 'title')
+    .by('Length', ['body.length'])
+    .by('Author', ['author', 'title'])
+
+  var post = {
+    title: 'a title',
+    body: 'lorem ipsum',
+    author: 'julian'
+  }
+
+  posts.put('1337', post, function (err) {
+    t.error(err)
+
+    posts.byTitle.get(['a title'], onPost)
+    posts.byLength.get([11], onPost)
+    posts.byAuthor.get(['julian', 'a title'], onPost)
+
+    function onPost (err, _post) {
+      t.error(err)
+      t.deepEqual(_post, post)
+    }
+  })
+})
